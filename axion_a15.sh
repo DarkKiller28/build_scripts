5936d7a916b05667dd8d9c832f2772cd18dc1193
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-08-20 11:14:44 +0300

    Update axion_a15.sh

--- axion_a15.sh
+++ axion_a15.sh
@@ -9,7 +9,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/DarkKiller28/local_manifests_mi439-clo .repo/local_manifests -b axion-15-qpr2
+git clone https://github.com/DarkKiller28/local_manifests_Mi439 .repo/local_manifests -b a15-qpr2-axion
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -33,7 +33,7 @@ git clone https://github.com/germaniumsculk/certs
 # Have to add these flags in device.mk
 
 # Export
-export BUILD_USERNAME=Germanium 
+export BUILD_USERNAME=DarkKiller28
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 echo "======= Export Done ======"
