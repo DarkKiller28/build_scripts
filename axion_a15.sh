4f3836f1023d3efe16265efa25ef1b05ad500419
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-08-22 00:55:28 +0300

    Update axion_a15.sh

--- axion_a15.sh
+++ axion_a15.sh
@@ -23,12 +23,12 @@ echo "============="
 # Curl Signing Files
 git clone https://github.com/germaniumsculk/certs
 
-#SIGNING_KEY_PATH ?= certs
-#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
-#SIGNING_KEY_PATH ?= certs
-#RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
-#PRODUCT_DEFAULT_DEV_CERTIFICATE := $(RELEASE_KEY)
-#PRODUCT_OTA_PUBLIC_KEYS := $(RELEASE_KEY)
+SIGNING_KEY_PATH ?= certs
+RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
+SIGNING_KEY_PATH ?= certs
+RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := $(RELEASE_KEY)
+PRODUCT_OTA_PUBLIC_KEYS := $(RELEASE_KEY)
 
 # Have to add these flags in device.mk
 
