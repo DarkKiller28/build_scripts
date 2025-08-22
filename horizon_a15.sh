ad979f3044dc31bb5024ffda69291376036c7bf7
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-08-22 12:26:50 +0300

    Update horizon_a15.sh

--- horizon_a15.sh
+++ horizon_a15.sh
@@ -21,13 +21,6 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-SIGNING_KEY_PATH ?= certs
-RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
-SIGNING_KEY_PATH ?= certs
-RELEASE_KEY := $(SIGNING_KEY_PATH)/releasekey
-PRODUCT_DEFAULT_DEV_CERTIFICATE := $(RELEASE_KEY)
-PRODUCT_OTA_PUBLIC_KEYS := $(RELEASE_KEY)
-
 # Have to add these flags in device.mk
 
 # Export
