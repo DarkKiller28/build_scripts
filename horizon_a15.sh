164967b030328d49bee202210a8557ac8f6eb549
DarkKiller28 <oyunbaskani7728@gmail.com>
2025-08-22 23:35:44 +0300

    Update horizon_a15.sh

--- horizon_a15.sh
+++ horizon_a15.sh
@@ -10,7 +10,7 @@ echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/DarkKiller28/local_manifests_mi439-clo .repo/local_manifests -b horizondroid-15-qpr2
+git clone https://github.com/DarkKiller28/local_manifests_Mi439 .repo/local_manifests -b a15-qpr2-horizon
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
